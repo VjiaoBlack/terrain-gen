8e158f70115bc1989933bfdc2d78e42ac0f2cbf4
Victor Jiao <VjiaoBlack@gmail.com>
2018-06-20 13:35:33 -0700

    added function to auto convert between SDL_Color and uint32_t

--- src/TrECS/TrComponents/TrGraphicsComponent.hpp
+++ src/TrECS/TrComponents/TrGraphicsComponent.hpp
@@ -37,19 +37,30 @@ class TrGraphicsComponent : public MyComponent {
 
 template<class C>
 void TrGraphicsComponent::update(TrGame *game, C *entity) {
-  float sx, sy;
+  // TODO: consider OpenGL pipeline (or blending) for shading, etc.
 
-  SDL_RenderGetScale(game->m_SDLRenderer, &sx, &sy);
-  SDL_RenderSetScale(game->m_SDLRenderer, sz(K_DISPLAY_SCALE),
-                     sz(K_DISPLAY_SCALE));
+  // go through rect and mark those pixels into the TrMap's m_entityColor.
 
-  if (m_texture) {
-    SDL_RenderCopy(game->m_SDLRenderer, m_texture, nullptr, &(entity->m_rect));
-  } else {
-    SDL_SetRenderDrawColor(game->m_SDLRenderer, m_color.r, m_color.g, m_color.b,
-                           m_color.a);
-    SDL_RenderFillRect(game->m_SDLRenderer, &(entity->m_rect));
+  for (int r = entity->m_rect.y; r < entity->m_rect.h; r++) {
+    for (int c = entity->m_rect.x; c < entity->m_rect.w; c++) {
+      game->m_map->m_entityColor->at(r, c) = convertColorType(m_color);
+    }
   }
-  SDL_RenderSetScale(game->m_SDLRenderer, sx, sy);
+
+//  float sx, sy;
+
+//  SDL_RenderGetScale(game->m_SDLRenderer, &sx, &sy);
+//  SDL_RenderSetScale(game->m_SDLRenderer, sz(K_DISPLAY_SCALE),
+//                     sz(K_DISPLAY_SCALE));
+
+//  if (m_texture) {
+//    SDL_RenderCopy(game->m_SDLRenderer, m_texture, nullptr, &(entity->m_rect));
+//  } else {
+//    SDL_SetRenderDrawColor(game->m_SDLRenderer, m_color.r, m_color.g, m_color.b,
+//                           m_color.a);
+//    SDL_RenderFillRect(game->m_SDLRenderer, &(entity->m_rect));
+//  }
+
+//  SDL_RenderSetScale(game->m_SDLRenderer, sx, sy);
 }
 
