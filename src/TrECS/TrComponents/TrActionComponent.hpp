c81011b2e1330c9160823ab3a5f2ff6ba5c52a37
Victor Jiao <VjiaoBlack@gmail.com>
2018-06-26 22:36:02 -0700

    cleaned

--- src/TrECS/TrComponents/TrActionComponent.hpp
+++ src/TrECS/TrComponents/TrActionComponent.hpp
@@ -12,24 +12,9 @@ class TrFormula;
 
 using namespace std;
 
-/**
- * @brief interface for all physics components
- */
 class TrActionComponent : public TrComponent {
  public:
-  // indicates the current action of the entity
-  // note: currently, all possible actions are stored as formulas.
-  TrFormula* m_action;
-  map<TrItem *, int> m_progress;
 
-  /**
-   * @brief calculates the physics stuffs
-   */
   template<class C>
-  void update(TrGame *game, C *entity);
+  void update(TrGame *game, C *entity) {}
 };
-
-template<class C>
-void TrActionComponent::update(TrGame *game, C *entity) {
-  //
-}
