cc40b1f1d81f6e26c758d36a0894a2a2b39cfdf8
Victor Jiao <VjiaoBlack@gmail.com>
2018-06-26 23:12:21 -0700

    fix flaw in TrFormula

--- src/TrECS/TrFormulas/TrFormula.hpp
+++ src/TrECS/TrFormulas/TrFormula.hpp
@@ -15,9 +15,10 @@ class TrFormula {
  public:
   TrFormula() = default;
 
-  map<TrItem *, int> m_inputs;
-  map<TrItem *, int> m_outputs;
-  map<TrItem *, int> m_byproducts;
+  // TODO: be careful, can't use regular map with unordered objects
+//  map<TrItem, int> m_inputs;
+//  map<TrItem, int> m_outputs;
+//  map<TrItem, int> m_byproducts;
 
   virtual ~TrFormula() = default;
 };
